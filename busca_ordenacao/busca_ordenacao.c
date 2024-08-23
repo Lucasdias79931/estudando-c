644296548b954915cad0259787ac40e2acce636d
Lucasdias79931 <lucasdias26@hotmail.com.br>
2024-08-23 19:50:52 -0300

    busca binária

--- busca_ordenacao/busca_ordenacao.c
+++ busca_ordenacao/busca_ordenacao.c
@@ -3,6 +3,29 @@
 #include <stdbool.h>
 
 
+
+int buscaDC(int vet[], int size, int value){
+    if(value < 0 || value >= size) return 0;
+
+    int left = 0, right = size - 1;
+    int mid;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+
+        if (vet[mid] == value) {
+            return 1;  
+        } else if (vet[mid] < value) {
+            left = mid + 1;  
+        } else {
+            right = mid - 1;  
+        }
+    }
+
+    return 0;  
+}
+
+
 void bubbleSort(int vet[], int size){
     int i = 0, j = 0;
 
@@ -15,6 +38,8 @@ void bubbleSort(int vet[], int size){
             }
         }
     }
+
+
 }
 
 
@@ -38,5 +63,6 @@ int main(){
     }
     printf("}\n");
 
+    
     return 0;
 }
