07bd4c87c3a729cb258c42e387afa7837c24a1eb
Lucasdias79931 <lucasdias26@hotmail.com.br>
2024-09-05 19:09:17 -0300

    busca binaria com remoção em str

--- functions/main.c
+++ functions/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 void removeName(char nomes[][10], int *numNomes, const char nomeParaRemover[]) {
     int i;
     for (i = 0; i < *numNomes; i++) {
@@ -15,21 +16,49 @@ void removeName(char nomes[][10], int *numNomes, const char nomeParaRemover[]) {
     }
 }
 
+// Busca binária. Usar somente se o vetor estiver ordenado
+void removeNameB(char nomes[][10], int len, const char nomeParaRemover[]) {
+    if(len < 1) return;
+
+    int esquerda = 0;
+    int direita = len - 1;
+
+    while(esquerda <= direita){
+        int mid = (esquerda + direita) / 2;
+
+        if(strcmp(nomes[mid], nomeParaRemover) == 0){
+            strcpy(nomes[mid], "");  // Limpa a string
+            return;
+        } else if(strcmp(nomes[mid], nomeParaRemover) < 0){
+            esquerda = mid + 1;
+        } else {
+            direita = mid - 1;
+        }
+    }
+
+    return;
+}
+
+
 int main() {
-    char nomes[][10] = {"lucas", "rita", "josé"};
-    int numNomes = 3;
+    char nomes[][10] = {"xela", "bernado","claudio","aline","ziery"};
+ 
+    int size = sizeof(nomes) / sizeof(nomes[0]);
+
+   
+   
 
     printf("Nomes atuais:\n");
-    for (int i = 0; i < numNomes; i++) {
+    for (int i = 0; i < size; i++) {
         printf("%s\n", nomes[i]);
     }
 
-    char nomeParaRemover[] = "rita";
-    removeName(nomes, &numNomes, nomeParaRemover);
+    char nomeParaRemover[] = "xela";
+    removeName(nomes, &size, nomeParaRemover);
 
     printf("\nNomes após a remoção:\n");
-    for (int i = 0; i < numNomes; i++) {
-        printf("%s\n", nomes[i]);
+    for (int i = 0; i < size; i++) {
+       if(strcmp(nomes[i],"") != 0) printf("%s\n", nomes[i]);
     }
 
     return 0;
