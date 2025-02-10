408306b2c4afe06e9c0c61e3510e24b651fe7654
Lucasdias79931 <lucasdias26@hotmail.com.br>
2025-02-10 19:25:52 -0300

    lista circular

--- ED/LISTA/Lista_circular.c
+++ ED/LISTA/Lista_circular.c
@@ -3,21 +3,66 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef struct  Data {
-    char *book;
-    int year;
-} Data;
+
 
 typedef struct No {
+    int number;
     struct No *previous;
-    Data *data;
     struct No *next;
 } No;
 
 typedef struct List{
-    struct No *previous;
+    struct No *ini;
     struct No *end;
     int size;
 }List;
+
+List *initialize() {
+    List *newList = malloc(sizeof(List));
+    if (!newList) {
+        perror("ERROR allocating Data");
+        exit(1);
+    }
+    newList->ini = NULL;
+    newList->end = NULL;
+    newList->size = 0;
+    return newList;
+}
+
+void push(List *list, int number) {
+    if (!list) {
+        perror("ERROR: List does not exist");
+        exit(1);
+    }
+
+    No *newNode = malloc(sizeof(No));
+    if (!newNode) {
+        perror("ERROR allocating Data");
+        exit(1);
+    }
+
+    newNode->number = number;
+
+    if (list->size == 0) {
+        
+        newNode->next = newNode;
+        newNode->previous = newNode;
+        list->ini = newNode;
+        list->end = newNode;
+    } else {
+        
+        newNode->previous = list->end;
+        newNode->next = list->ini;
+
+        list->end->next = newNode;
+        list->ini->previous = newNode;
+        list->end = newNode;
+    }
+
+    list->size++; 
+}
+
+
+
 
 
