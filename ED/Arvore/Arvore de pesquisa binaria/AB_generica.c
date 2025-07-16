f776de422358c9a608446e25dee0094f835b2f55
Lucasdias79931 <lucasdias26@hotmail.com.br>
2025-07-16 10:47:03 -0300

    estruturando arvore binaria  generica

--- ED/Arvore/Arvore de pesquisa binaria/AB_generica.c
+++ ED/Arvore/Arvore de pesquisa binaria/AB_generica.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <time.h>
 
-
 typedef enum Type {
     Integer = 0,
     String,
@@ -14,48 +13,114 @@ typedef enum Type {
     Short_int
 } Type;
 
-
-typedef struct Node{
+typedef struct Node {
     void *data;
     struct Node *left;
     struct Node *right;
-}Node;
+} Node;
 
-typedef struct Tree
-{
+typedef struct Tree {
     Node *root;
     Type type;
-}Tree;
+} Tree;
 
-Tree *createTree(Type type){
+Tree *createTree(Type type) {
     Tree *tree = malloc(sizeof(Tree));
-    
-    if(tree == NULL){
-        fprintf(stderr, "Error in allocation memory for a new tree\nExiting...");
+    if (!tree) {
+        fprintf(stderr, "Error allocating memory for tree\n");
         exit(1);
     }
-
     tree->type = type;
-    tree->root->left = NULL;
-    tree->root->right = NULL;
-    tree->root->data = NULL;
+    tree->root = NULL;
     return tree;
 }
 
-/*
-    In 'compare', case return 0 is left case 1 is right
-*/
-const short int compare(const void *data1, const void *data2, const Type type){
 
+short int compare(const void *data1, const void *data2, const Type type) {
+    if (!data1 || !data2) {
+        fprintf(stderr, "Data is NULL in compare\n");
+        exit(1);
+    }
+
+    switch (type) {
+        case Integer:
+            return (*(int *)data1 - *(int *)data2);
+        case String:
+            return strcmp((const char *)data1, (const char *)data2);
+        case Char:
+            return (*(char *)data1 - *(char *)data2);
+        case Float:
+            return (*(float *)data1 > *(float *)data2) - (*(float *)data1 < *(float *)data2);
+        case Double:
+            return (*(double *)data1 > *(double *)data2) - (*(double *)data1 < *(double *)data2);
+        case Short_int:
+            return (*(short int *)data1 - *(short int *)data2);
+        default:
+            fprintf(stderr, "Unknown type in compare\n");
+            exit(1);
+    }
 }
 
-void insert(Node **root, const void *data, const Type type){
-    Node *node = NULL;
-
+short int sizeData(const void *data, Type type) {
+    switch (type) {
+        case Integer:    return sizeof(int);
+        case String:     return strlen((const char *)data) + 1;
+        case Char:       return sizeof(char);
+        case Float:      return sizeof(float);
+        case Double:     return sizeof(double);
+        case Short_int:  return sizeof(short int);
+        default:
+            fprintf(stderr, "Unknown type in sizeData\n");
+            exit(1);
+    }
 }
 
 
+void copyData(void **destineData, const void *originData, short int size) {
+    *destineData = malloc(size);
+    if (!*destineData) {
+        fprintf(stderr, "Error allocating memory for data\n");
+        exit(1);
+    }
+    memcpy(*destineData, originData, size);
+}
 
-int main(){
+void insert(Tree *tree, const void *data, Type type) {
+    if (!tree || type != tree->type) {
+        fprintf(stderr, "Tree is null or type mismatch\n");
+        exit(1);
+    }
 
+    Node *new_node = malloc(sizeof(Node));
+    if (!new_node) {
+        fprintf(stderr, "Error allocating memory for node\n");
+        exit(1);
+    }
+
+    copyData(&new_node->data, data, sizeData(data, type));
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    if (tree->root == NULL) {
+        tree->root = new_node;
+        return;
+    }
+
+    Node *current = tree->root;
+    Node *parent = NULL;
+
+    while (current) {
+        parent = current;
+        if (compare(data, current->data, type) < 0) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+
+    if (compare(data, parent->data, type) < 0) {
+        parent->left = new_node;
+    } else {
+        parent->right = new_node;
+    }
 }
